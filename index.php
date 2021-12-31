a34a671ae9b9eef1fd959bb94e2fd708a3556ca1
las157tu1212 <kame777w@gmail.com>
2021-12-31 17:07:16 +0900

    index変更

--- index.php
+++ index.php
@@ -17,5 +17,23 @@
     <a>うちの子のあれこれを記録する為のページ</a>
   </header>
   <a href="php/main.php">メインページ</a>
+  
+<?php
+require 'vendor/autoload.php';
+$email = new SendGridMailMail();
+$email->setFrom("test@example.com", "送信者A");
+$email->setSubject("TestMail漢字");
+$email->addTo("kame777w@gmail.com", "受信者B");
+$email->addContent("text/plain", "日本語 English");
+$sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "n";
+    print_r($response->headers());
+    print $response->body() . "n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."n";
+}
+?>
 </body>
 </html>
